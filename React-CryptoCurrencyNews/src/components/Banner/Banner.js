82ef51b6a548555fd62a20f5d4f675d430022d41
dhanmeetsingh <42151190+dhanmeetsingh@users.noreply.github.com>
2023-04-09 21:59:08 +0200

    Add files via upload

---
+++ React-CryptoCurrencyNews/src/components/Banner/Banner.js
@@ -0,0 +1,71 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import Carousel from "./Carousel";
+
+const useStyles = makeStyles((theme) => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpg)",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    height: "600px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bannerContent: {
+    width: "80%",
+    height: "80%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+function Banner() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+              color: "white",
+            }}
+          >
+            Crypto Hunter
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "white",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+        <Carousel />
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
